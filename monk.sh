b0b1fe3abec6b5cccc494732f2ad4bc729890321
xToNouSou <xtonousou96@gmail.com>
2016-07-28 04:35:21 +0300

    add monk

---
+++ monk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Displays and removes orphans.
+## Cleans pacman cache/db directory.
+## Optimizes database.
+
+pacman -Rscn $(pacman -Qtdq)
+pacman -Sc
+pacman-optimize && sync
+updatedb
+
+exit 0
