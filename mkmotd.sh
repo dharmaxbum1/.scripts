7048636b25552ec1d4fdf113777e5731104bf838
xToNouSou <xtonousou96@gmail.com>
2016-08-01 07:01:40 +0300

    minor updates

--- mkmotd.sh
+++ mkmotd.sh
@@ -6,11 +6,11 @@ motd="/etc/motd"
 HOSTNAME=`uname -n`
 KERNEL=`uname -r`
 ARCH=`uname -m`
-## Some useful commands to use
 # Memory
 MEMUSED=`/bin/free   -m | /bin/grep Mem | /bin/awk {'print $3'}`
 MEMFREE=`/bin/free   -m | /bin/grep Mem | /bin/awk {'print $4'}`
 # Disk
+# get the load averages
 HDDUSED=`/bin/df -h | /bin/grep sda6 | /bin/awk {'print $3'} | /bin/sed 's/[^0-9]*//g'`
 HDDFREE=`/bin/df -h | /bin/grep sda6 | /bin/awk {'print $4'} | /bin/sed 's/[^0-9]*//g'`
 # Battery
@@ -27,6 +27,13 @@ W="\033[01;37m"
 B="\033[01;34m"
 R="\033[01;31m"
 X="\033[00;37m"
+## Time
+upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+let secs=$((${upSeconds}%60))
+let mins=$((${upSeconds}/60%60))
+let hours=$((${upSeconds}/3600%24))
+let days=$((${upSeconds}/86400))
+UPTIME=`printf "%d days, %02dh %02dm %02ds " "$days" "$hours" "$mins" "$secs"`
 ## THE OUTPUT
 clear 											    		        >  $motd # to clear the screen when showing up
 echo -e "$R#=============================================================================#" 		        >> $motd
@@ -39,17 +46,18 @@ echo -e "$W      /__/\_ \ |____| \____/\____|__  /\____/|____/_______  /\____/|_
 echo -e "$W            \/                      \/                    \/"   				        >> $motd
 echo -e "$X"										   		        >> $motd
 echo -e "$R#=============================================================================#" 		        >> $motd
-echo -e "	$W Welcome $B $USER $W to $B $HOSTNAME"   						        >> $motd
-echo -e "	$R ARCH   $W= $ARCH" 							  	       	        >> $motd
-echo -e "	$R KERNEL $W= $KERNEL"							   		        >> $motd
+echo -e "	$R USER............:$W $USER"						        		>> $motd
+echo -e "	$R HOSTNAME........:$W $HOSTNAME"						        	>> $motd
+echo -e "	$R ARCH............:$W $ARCH" 							  	       	>> $motd
+echo -e "	$R KERNEL..........:$W $KERNEL"							   		>> $motd
 echo -e "$R#=============================================================================#" 		        >> $motd
-echo -e "	$R CPU TEMPERATURE $W= $B$TEMPCPU"$CEL"                 $B$TEMPHDD"$CEL" $W =$R HDD TEMPERATURE">> $motd
-echo -e "	$R HDD USAGE       $W= $HDDUSED"GiB"             $HDDFREE"GiB"$W =$R HDD FREE" 		        >> $motd
-echo -e "	$R MEMORY USAGE    $W= $MEMUSED"MiB"         $MEMFREE"MiB"$W =$R MEMORY FREE"  		        >> $motd
-echo -e "	$R BATTERY 	 $W= $BAT    	        $SOUND"%"$W =$R SOUND VOLUME"			        >> $motd
+echo -e "	$R CPU TEMPERATURE.:$W $TEMPCPU"$CEL"            $R HDD TEMPERATURE.:$W $TEMPHDD"$CEL""		>> $motd
+echo -e "	$R HDD USAGE.......:$W $HDDUSED"GiB"         $R HDD FREE........:$W $HDDFREE"GiB"" 		>> $motd
+echo -e "	$R MEMORY USAGE....:$W $MEMUSED"MiB"        $R MEMORY FREE.....:$W $MEMFREE"MiB""               >> $motd
+echo -e "	$R BATTERY.........:$W $BAT    	  $R SOUND VOLUME....:$W $SOUND"%""			        >> $motd
 echo -e "$R#=============================================================================#" 		        >> $motd
 echo -e "$X" 										    		        >> $motd
-echo -e "$W                          MAY THE FORCE BE WITH YOU"   		                                >> $motd
+echo -e "$W                           MAY THE FORCE BE WITH YOU"   		                                >> $motd
 echo -e "$R     This is a private system that you are not to give out access to anyone"   		        >> $motd
 echo -e "$R     without permission from the admin. No illegal files or activity. Stay,"   		        >> $motd
 echo -e "$R     in your home directory, keep the system clean, and make regular backups."   		        >> $motd
