0b771e37f70761509ebf1c03aff4cb0e862063d1
xToNouSou <xtonousou96@gmail.com>
2016-08-04 04:06:14 +0300

    add run as root check

--- monk.sh
+++ monk.sh
@@ -4,9 +4,14 @@
 ## Cleans pacman cache/db directory.
 ## Optimizes database.
 
+if [ "$EUID" -ne 0 ]
+  then printf "\e[31mPlease run as root\n"
+  exit
+fi
+
 pacman -Rscn $(pacman -Qtdq)
 pacman -Sc
 pacman-optimize && sync
 updatedb
 
-exit 1
+exit
