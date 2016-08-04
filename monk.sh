178a5716b135bac98909a8c3e694057a51a8c0a2
xToNouSou <xtonousou96@gmail.com>
2016-08-04 04:18:52 +0300

    add colors and spaces

--- monk.sh
+++ monk.sh
@@ -4,14 +4,23 @@
 ## Cleans pacman cache/db directory.
 ## Optimizes database.
 
+# Colors
+R="\033[01;31m"
+G="\033[01;32m"
+B="\033[01;34m"
+
 if [ "$EUID" -ne 0 ]
-  then printf "\e[31mPlease run as root\n"
+  then printf "${R}Please run as root\n"
   exit
 fi
 
+printf "${B}---> ${G}pacman -Rscn $(pacman -Qtdq)\n"
 pacman -Rscn $(pacman -Qtdq)
+printf "\n${B}---> ${G}pacman -Sc\n"
 pacman -Sc
+printf "\n${B}---> ${G}pacman-optimize && sync\n"
 pacman-optimize && sync
+printf "\n${B}---> ${G}updatedb\n"
 updatedb
 
 exit
