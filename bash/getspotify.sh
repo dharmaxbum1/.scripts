3f081e5cd4430c15d92d0a0346c64ea3f0620e67
xtonousou <xtonousou96@gmail.com>
2016-09-12 01:12:49 +0300

    change method to get spotify info

---
+++ bash/getspotify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# get spotify current playing song's title, artist and album
+ 
+if [[ $(pidof spotify) ]]; then
+
+   if [ $# -lt 1 ]; then
+      echo "This script needs at least one command-line argument!"
+      exit
+   fi
+
+   case "$1" in
+      artist)
+         echo `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$` 
+         ;;
+      title)
+         echo `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+         ;;
+      album)
+         echo `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "album"|egrep -v "album"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+         ;;
+   esac
+   
+fi
