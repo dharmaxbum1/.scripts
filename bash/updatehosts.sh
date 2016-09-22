82d9e269d6b34f93901ebb71545f34df9636c4fc
xtonousou <xtonousou96@gmail.com>
2016-09-22 18:00:14 +0300

    update updatehosts

--- bash/updatehosts.sh
+++ bash/updatehosts.sh
@@ -4,8 +4,8 @@
 ## Dependencies: wget, curl, printf
 
 # Vars
-HOSTS='https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts'
-PATH_TO_HOSTS='/etc'
+HOSTS="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+PATH_TO_HOSTS="/etc"
 
 # Colors vars
 GREEN="\033[1;32m"
@@ -18,25 +18,31 @@ BACKUP_OLD_HOSTS=`cp /etc/hosts /etc/hosts.old 2> /dev/null`
 REMOVE_OLD_HOSTS=`rm -f /etc/hosts 2> /dev/null`
 GET_HOSTS=`wget -q $HOSTS -P $PATH_TO_HOSTS 2> /dev/null`
 
+function check_su() {
+
+   if [ "$EUID" -ne 0 ]; then
+      echo -e "$RED \berror:$NORMAL You cannot perform this operation unless you are root."
+      exit
+   fi
+}
+
 function main() {
 
    if [[ -f /etc/hosts ]]; then
       if [ "${HTTP_CODE}" = "200" ]; then
+	     echo -e "$GREEN \bWebsite is up and running"
+         sleep 0.5
          ${BACKUP_OLD_HOSTS}
+         echo -e "$NORMAL \bKeeping backup of the previous version..."
+         sleep 0.5
          ${REMOVE_OLD_HOSTS}
+         echo -e "$NORMAL \bFetching..."
          ${GET_HOSTS}
-         echo -e "$GREEN \bDone$NORMAL"
+         sleep 0.5
+         echo -e "$NORMAL \bPlease reboot your computer for these changes to take effect"
       else
          echo -e "$RED \bSource unreachable$NORMAL"
       fi
-   fi
-}
-
-function check_su() {
-
-   if [ "$EUID" -ne 0 ];
-      then echo -e "$RED \berror:$NORMAL You cannot perform this operation unless you are root."
-      exit
    fi
 }
 
