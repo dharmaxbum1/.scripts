88c0b9f147d7303917cbfde41b4992fc342951c4
Sotiris Roussis <xtonousou@gmail.com>
2016-12-27 16:44:46 +0000

    Updated dir

---
+++ bash/torimon.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+TV="HDMI1"
+LAPTOP="eDP1"
+_1080_="1920x1080"
+
+if [[ -z "$1" ]]; then
+  show_usage
+fi
+
+function show_usage() {
+  
+  echo "torimon tv"
+  echo "torimon laptop"
+  echo "torimon mirror"
+  echo "torimon extend"
+  exit
+}
+
+function check_xrandr() {
+
+  ! hash xrandr && echo "Please install xrandr first." && exit 	
+}
+
+function __init__() {
+	
+  check_xrandr
+  
+  case "$1" in
+    "tv")
+      xrandr --output "${TV}" --mode "${_1080_}" --output "${LAPTOP}" --off
+    ;;
+    "laptop")
+      xrandr --output "${LAPTOP}" --mode "${_1080_}" --output "${TV}" --off
+    ;;
+    "mirror")
+      xrandr --output "${LAPTOP}" --mode "${_1080_}" --output "${TV}" --mode "${_1080_}" --same-as "${LAPTOP}"
+    ;;
+    "extend")
+      xrandr --output "${TV}" --mode "${_1080_}" --right-of "${LAPTOP}" --mode "${_1080_}"
+    ;;
+    *)
+      show_usage
+    ;;
+  esac
+  
+  exit
+}
+
+__init__ "$1"
