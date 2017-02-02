a546ca162602306012ea63594a4025383da02988
Sotiris Roussis <xtonousou@gmail.com>
2017-02-02 18:01:29 +0200

    Added colors, more pkg mngrs, checking functions. etc...

--- ruby/steroids.rb
+++ ruby/steroids.rb
@@ -1,51 +1,76 @@
 #!/usr/bin/ruby -w
-# -w : more warnings
 
-#raise "Must run as root" unless Process.uid == 0
+class String
+def red;            "\e[31m#{self}\e[0m" end
+def green;          "\e[32m#{self}\e[0m" end
+def blue;           "\e[34m#{self}\e[0m" end
+def magenta;        "\e[35m#{self}\e[0m" end
+def cyan;           "\e[36m#{self}\e[0m" end
+
+def bold;           "\e[1m#{self}\e[22m" end
+def italic;         "\e[3m#{self}\e[23m" end
+def underline;      "\e[4m#{self}\e[24m" end
+end
 
 def helpMsg()
 
   puts  " -h | --help )",
-        "   Help",
-        " -d | --distro | --distribution )",
-        "   Accepted distros: arch, debian, manjaro, ubuntu"
-  exit
+        "   Help".magenta ,
+        " -p | --package-manager )",
+        "   Accepted package managers: apt, aptitude, dnf, zypper, pacman, portage, rpm, yum".magenta
 end
 
-def installation(packageManager)
+def init(manager, command)
 
-  puts "Start post installation? [y/n]"
+  `hash '#{manager}' &> /dev/null || function value() { return 109; }; value &> /dev/null`
+  if $?.exitstatus  == 109 then puts "#{manager} is not your package manager".cyan; exit(1) end
+  puts "Start post installation? [y/n]".bold
   input = STDIN.gets.chomp
   case input
     when "y" , "Y"
       packages = "firefox gimp" # insert package names here
-      exec "sudo #{packageManager} #{packages}"
-    when "n" , "N" then puts "Aborting post installation"
+      exec "#{command} #{packages}"
+    when "n" , "N" then puts "Aborting post installation".bold
     else
-      puts "Invalid input. Aborting"
-      exit
+      puts "Invalid input. Aborting".red
+      exit(1)
   end
 end
 
-def main(arg1, arg2)
+def main(option, manager)
+
+  abort("Must run as root".red.underline) unless Process.uid == 0
 
   case
-    when (arg1.nil? or arg1.empty?) then helpMsg
-    when (arg2.nil? or arg2.empty?) then helpMsg
+    when (option.nil? or option.empty?) then helpMsg; exit(1)
+    when (manager.nil? or manager.empty?) then helpMsg; exit(1)
     else nil
   end
 
-  case arg1
-    when "-h", "--help" then helpMsg
-    when "-d" , "--distro" , "--distribution"
-    case arg2
-      when "debian" , "ubuntu" then installation("apt-get install")
-      when "arch" , "manjaro" then installation("pacman -S")
-      else puts "'#{arg2}' is not supported yet"
-    end
-    else helpMsg
+  case option
+    when "-h", "--help" then helpMsg; exit(0)
+    when "-p" , "--package-manager"
+      case manager
+        when "apt" then init("apt", "apt-get install")
+        when "aptitude" then init("aptitude", "aptitude install")
+        when "dnf" then init("dnf", "dnf install")
+        when "zypper" then init("zypper", "zypper in")
+        when "pacman" then init("pacman", "pacman -S")
+        when "portage" then init("emerge", "emerge -s")
+        when "rpm" then init("rpm", "rpm -ivh")
+        when "yum" then init("yum", "yum install")
+        else puts "#{manager} package manager is not supported yet".blue.bold; exit(1)
+      end
+    else helpMsg; exit(1)
   end
 end
 
-main(ARGV[0], ARGV[1])
+# suppress interrupt exception messages by handling
+begin
+  main(ARGV[0], ARGV[1])
+rescue Interrupt
+  puts " Aborting".green.bold
+end
+
+
 
