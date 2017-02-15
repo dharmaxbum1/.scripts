6140eaa00247e1036964498e2da5f40c68f4ce4b
Sotiris Roussis <xtonousou@gmail.com>
2017-02-15 15:38:17 +0200

    Whee, good night.

--- bash/gitit.sh
+++ bash/gitit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ -z "${1}" ]]; then echo "gitit BRANCH-NAME COMMIT-MESSAGE"; exit 1; fi
-if [[ -z "${2}" ]]; then echo "gitit BRANCH-NAME COMMIT-MESSAGE"; exit 1; fi
-
 BRANCH="${1}"
 MSG="${2}"
+
+if [[ -z "${BRANCH}" ]]; then echo "gitit BRANCH-NAME COMMIT-MESSAGE"; exit 1; fi
+if [[ -z "${MSG}" ]]; then echo "gitit BRANCH-NAME COMMIT-MESSAGE"; exit 1; fi
+if [[ "${MSG}" = "RANDOM" ]]; then MSG=$(echo "$(curl -s http://whatthecommit.com/index.txt)"); fi
 
 git pull origin "${BRANCH}"
 git add *
